0bfb8858097ad90800df76ed6c4292d49fa46df3
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-10-27 07:02:29 +0700

    Updating kotlin generic fragment.

--- app/src/main/java/com/taufik/kotlinhero/ui/home/kotlingeneric/fragment/KotlinGenericFragment.kt
+++ app/src/main/java/com/taufik/kotlinhero/ui/home/kotlingeneric/fragment/KotlinGenericFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.taufik.kotlinhero.databinding.FragmentKotlinGenericBinding
 
 class KotlinGenericFragment : Fragment() {
@@ -22,5 +23,16 @@ class KotlinGenericFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        actionToHome()
+    }
+
+    private fun actionToHome() {
+        binding.apply {
+            cardBack.setOnClickListener {
+                val intentAction = KotlinGenericFragmentDirections.actionKotlinGenericFragmentToNavHome()
+                findNavController().navigate(intentAction)
+            }
+        }
     }
 }
