8b14a79b699dee3b22a3047531c80b8b0b248b1d
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-01-09 11:49:33 +0700

    Updating project.

--- app/src/main/java/com/taufik/kotlinhero/model/category/CategoryItem.kt
+++ app/src/main/java/com/taufik/kotlinhero/model/category/CategoryItem.kt
@@ -1,7 +1,11 @@
 package com.taufik.kotlinhero.model.category
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class CategoryItem(
     val categoryIcon: Int,
     val categoryName: String,
     val categoryNumber: String
-)
+): Parcelable
