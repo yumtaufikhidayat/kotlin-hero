83e6f350fd43c4acc793e3ac3942107258f725f7
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-01-11 21:13:51 +0700

    Adding press back again to exit.

--- app/src/main/java/com/taufik/kotlinhero/ui/MainActivity.kt
+++ app/src/main/java/com/taufik/kotlinhero/ui/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.taufik.kotlinhero.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -13,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     private lateinit var navController: NavController
+    private var doubleBackToExitPressedOnce = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,5 +34,20 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             navBottom.setupWithNavController(navController)
         }
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Klik sekali lagi untuk keluar", Toast.LENGTH_SHORT).show()
+
+        Handler(Looper.getMainLooper())
+            .postDelayed({
+                doubleBackToExitPressedOnce = false
+            }, 2000)
     }
 }
