63d28d7d8bd2f4f6975b432bd032f261c98f51ca
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-01-12 06:07:38 +0700

    Removing double on back pressed to exit feature.

--- app/src/main/java/com/taufik/kotlinhero/ui/MainActivity.kt
+++ app/src/main/java/com/taufik/kotlinhero/ui/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.taufik.kotlinhero.ui
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -16,7 +13,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     private lateinit var navController: NavController
-    private var doubleBackToExitPressedOnce = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,20 +30,5 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             navBottom.setupWithNavController(navController)
         }
-    }
-
-    override fun onBackPressed() {
-        if (doubleBackToExitPressedOnce) {
-            super.onBackPressed()
-            return
-        }
-
-        this.doubleBackToExitPressedOnce = true
-        Toast.makeText(this, "Klik sekali lagi untuk keluar", Toast.LENGTH_SHORT).show()
-
-        Handler(Looper.getMainLooper())
-            .postDelayed({
-                doubleBackToExitPressedOnce = false
-            }, 2000)
     }
 }
