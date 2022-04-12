d875f3b23ce04c6cab2e52a488958cce89404f9a
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-04-12 20:57:14 +0700

    Add double back pressed to exit app.

--- app/src/main/java/com/taufik/kotlinhero/ui/MainActivity.kt
+++ app/src/main/java/com/taufik/kotlinhero/ui/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.taufik.kotlinhero.ui
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -17,8 +14,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     private lateinit var navController: NavController
-    private var doubleBackToExitPressedOnce = false
-    private val delayTime = 2000L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,20 +53,5 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             navBottom.visibility = View.GONE
         }
-    }
-
-    override fun onBackPressed() {
-        if (doubleBackToExitPressedOnce) {
-            super.onBackPressed()
-            return
-        }
-
-        this.doubleBackToExitPressedOnce = true
-        Toast.makeText(this, "Klik sekali lagi untuk keluar", Toast.LENGTH_SHORT).show()
-
-        Handler(Looper.getMainLooper())
-            .postDelayed({
-                doubleBackToExitPressedOnce = false
-            }, delayTime)
     }
 }
