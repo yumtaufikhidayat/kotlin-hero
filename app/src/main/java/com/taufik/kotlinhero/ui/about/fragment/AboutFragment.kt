0b9ab84a9061b7aae66a6185936da419caedd464
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-04-18 05:52:10 +0700

    Update binding view.

--- app/src/main/java/com/taufik/kotlinhero/ui/about/fragment/AboutFragment.kt
+++ app/src/main/java/com/taufik/kotlinhero/ui/about/fragment/AboutFragment.kt
@@ -5,22 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.taufik.kotlinhero.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
+
+    private var _binding: FragmentAboutBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        _binding = null
     }
 }
