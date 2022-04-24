1ca1512139a0305a502bffac4eb8714b5fa687e8
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-04-24 08:40:14 +0700

    Update FavoriteFragment.kt.

--- app/src/main/java/com/taufik/kotlinhero/ui/favorite/fragment/FavoriteFragment.kt
+++ app/src/main/java/com/taufik/kotlinhero/ui/favorite/fragment/FavoriteFragment.kt
@@ -5,22 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.taufik.kotlinhero.R
+import com.taufik.kotlinhero.databinding.FragmentFavoriteBinding
 
 class FavoriteFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setFavoriteData()
+    }
+
+    private fun setFavoriteData() {
+        binding.apply {
+            toolbarFavorite.toolbarTitle.text = getString(R.string.icFavorite)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        _binding = null
     }
 }
