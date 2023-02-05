46ecd830ecbb625d5cd8211414405157f0815750
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2023-02-05 11:02:31 +0700

    Implement common for toast.

---
+++ app/src/main/java/com/taufik/kotlinhero/utils/ToastUtils.kt
@@ -0,0 +1,10 @@
+package com.taufik.kotlinhero.utils
+
+import android.content.Context
+import android.widget.Toast
+
+object ToastUtils {
+    fun showToast(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+}
